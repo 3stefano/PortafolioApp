b35b754f3517c2414cd317ec1cd61a371857eaa3
3stefano <57874032+3stefano@users.noreply.github.com>
2021-10-29 14:06:11 -0300

    Funcionalidades
    Colores por status.
    forms arreglado por los campos.
    fecha normalizada.

--- arriendosv2/accounts/forms.py
+++ arriendosv2/accounts/forms.py
@@ -18,14 +18,17 @@ class ReservaForm(ModelForm):
         "nombre_completo": "Nombre Completo",
         "fechareserva":"Fecha para reservar",
         "departamento_id_depto": "ID Departamento que desea:",
-        "servicios_extra_tipo_servicio":"¿Desea Servicio Extra?"
+        "servicios_extra_tipo_servicio":"Desea Servicio Extra?"
    		}
 		widgets = {	
 					'rutcliente': forms.TextInput(attrs={'class':'form-control'}),
 					'nombre_completo': forms.TextInput(attrs={'class':'form-control'}),
-					'fechareserva':forms.DateInput(attrs={'class':'form-control'}),
+					'fechareserva':forms.DateInput(format=("%Y-%m-%d"),attrs={'class': 'form-control', 
+               'placeholder': 'Select a date','type': 'date'}),
 					'correo': forms.EmailInput(attrs={'class':'form-control'}),
 					'telefono': forms.TextInput(attrs={'class':'form-control'}),
+					'departamento_id_depto':forms.Select(attrs={'class': 'form-control'}),
+					'servicios_extra_tipo_servicio':forms.Select(attrs={'class': 'form-control'}),
 					
 			}
 
